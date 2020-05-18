960d09540ff53e8f00a28f7927761e3e48c6c2a2
Borys Palka <borys.palka@codete.com>
2020-05-18 17:14:10 +0200

    implement BEM

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export function ProgressBar({ className = "", percent = 0, trackRemaining = false }) {
     percent = (trackRemaining) ? 100 - percent : percent;
-    return (<div className={`ProgressBar ${className} ${trackRemaining ? 'reverse' : ''}`}>
-        <div style={{ width: `${percent}%` }}></div>
+    return (<div className={`progress ${className} ${trackRemaining ? 'progress--reverse' : ''}`}>
+        <div className={`progress__bar progress--color-red`} style={{ width: `${percent}%` }}></div>
     </div>);
 }
